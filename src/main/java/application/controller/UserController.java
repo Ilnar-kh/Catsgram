5e1e3d765bf99e1c7c38413ace03414738a99277
Ilnar Khusainov <ilnarrkhusainov@yandex.ru>
2025-06-23 22:12:19 +0500

    Разделение обработки запросов и бизнес-логики

---
+++ src/main/java/application/controller/UserController.java
@@ -0,0 +1,33 @@
+package application.controller;
+
+import model.User;
+import org.springframework.web.bind.annotation.*;
+import service.UserService;
+
+import java.util.Collection;
+
+@RestController
+@RequestMapping("/users")
+public class UserController {
+
+    private final UserService userService;
+
+    public UserController(UserService userService) {
+        this.userService = userService;
+    }
+
+    @GetMapping
+    public Collection<User> findAll() {
+        return userService.findAll();
+    }
+
+    @PostMapping
+    public User create(@RequestBody User user) {
+        return userService.create(user);
+    }
+
+    @PutMapping
+    public User update(@RequestBody User newUser) {
+        return userService.update(newUser);
+    }
+}
